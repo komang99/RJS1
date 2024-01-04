dc4f8882b97d7a911f347faa663f46e5050b0020
I Nyoman Sweta <mrsveta@gmail.com>
2024-01-04 13:52:06 +0800

    Perbaikan di App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ini pesan yang dibuat manual.
         </p>
         <a
           className="App-link"
